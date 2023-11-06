3df4e18972fcaba672380da2e0cae386532eefb2
Hart Traveller <hart@MBP.local>
2023-11-05 22:30:34 -0500

    pasc/core: consolidate code for temp debugging

--- pasc/core.py
+++ pasc/core.py
@@ -1,42 +1,65 @@
+# from pydantic.dataclasses import dataclass
+# from pasc import parallel
+
 # external imports
 
 import asyncio
 from typing import Optional
 from pydantic import BaseModel
 from pssm import secrets
+from time import perf_counter
+from datetime import datetime
+from aiohttp import ClientSession, ClientTimeout
+from typing import Optional
 
 # internal imports
-from pasc import parallel
-
-# from pydantic.dataclasses import dataclass
-
-
-class ResponseResult:
-    pass
+from pasc.util import compute_timedelta_seconds
+from pasc.env import SCRAPESTACK_URL
 
 
-class BatchResults:
-    pass
+async def _async_request(
+    session: ClientSession,
+    url: str,
+    key: str,
+):
+    start = perf_counter()
+    try:
+        async with session.get(
+            SCRAPESTACK_URL,
+            params={
+                "access_key": key,
+                "url": url,
+            },
+        ) as resp:
+            html = await resp.text()
+            end = perf_counter()
+            elapsed = end - start
+            return html, url, resp.status, round(elapsed, 4)
+    except asyncio.TimeoutError:
+        end = perf_counter()
+        elapsed = end - start
+        return None, url, 408, round(elapsed, 4)
 
 
-class BatchStatistics:
-    pass
-
-
-class ResponseBatch(BaseModel):
-    pass
-
-    @property
-    def success(self) -> None:
-        pass
-
-    @property
-    def failure(self) -> None:
-        pass
-
-    @property
-    def results(self) -> None:
-        pass
+async def _async_batch_request(
+    urls: list[str],
+    key: str,
+    headers: Optional[dict] = None,
+    cookies: Optional[dict] = None,
+    timeout: Optional[int] = 100,
+) -> list:
+    batch_start_time = datetime.now()
+    async with ClientSession(
+        headers=headers, cookies=cookies, timeout=ClientTimeout(total=timeout)
+    ) as session:
+        tasks = []
+        for url in urls:
+            tasks.append(
+                asyncio.ensure_future(_async_request(session=session, url=url, key=key))
+            )
+        results = await asyncio.gather(*tasks)
+    batch_end_time = datetime.now()
+    return results, compute_timedelta_seconds(start=batch_start_time, end=batch_end_time)
 
 
 class Retriever:
@@ -99,3 +122,30 @@ if __name__ == "__main__":
     print(b)
     # * note: runs in terminal, not in notebook
     # ! RuntimeError: asyncio.run() cannot be called from a running event loop
+
+# class ResponseResult:
+#     pass
+
+
+# class BatchResults:
+#     pass
+
+
+# class BatchStatistics:
+#     pass
+
+
+# class ResponseBatch(BaseModel):
+#     pass
+
+#     @property
+#     def success(self) -> None:
+#         pass
+
+#     @property
+#     def failure(self) -> None:
+#         pass
+
+#     @property
+#     def results(self) -> None:
+#         pass
