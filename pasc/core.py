79dfe6e4f4c9c9512dda3f1d8f07b81f3755257b
Hart Traveller <hart@MBP.local>
2023-11-05 21:52:29 -0500

    pasc/core: add other params

--- pasc/core.py
+++ pasc/core.py
@@ -1,6 +1,15 @@
 from pssm import secrets
+from typing import Optional
+from pydantic import BaseModel
 
 
 class Retriever:
-    def __init__(self, token: str = secrets.get("scrapestack")) -> None:
+    def __init__(
+        self,
+        token: str = secrets.get("scrapestack"),
+        timeout: int = 20,
+        verbose: bool = False,
+        parser: Optional[BaseModel] = None,
+        cookies: Optional[dict] = None,
+    ) -> None:
         self.token = token
